f9b17c2ad02d71f998b0f71cce56f10876087166
mariohg81 <103214109+mariohg81@users.noreply.github.com>
2022-09-22 12:00:47 +0200

    Add files via upload

---
+++ Plot_Disp.R
@@ -0,0 +1,17 @@
+library(readr)
+disp <- read_csv("disp.csv")
+View(disp)
+
+fig <- plot_ly(disp,name = 'gfem_front_base', x = ~gfem_front_base_x, y = ~gfem_front_base_y, z = ~gfem_front_base_z, type = 'scatter3d', mode = 'markers', opacity = 1, line = list(width = 6, reverscale = FALSE))
+fig <- fig %>% add_trace(name = 'gfem_rear_base',x = ~gfem_rear_base_x, y = ~gfem_rear_base_y, z = ~gfem_rear_base_z, mode = 'markers', opacity = 1, line = list(width = 6, reverscale = FALSE))
+fig <- fig %>% add_trace(name = 'gfem_front_deform',x = ~gfem_front_deform_x, y = ~gfem_front_deform_y, z = ~gfem_front_deform_z, mode = 'markers', opacity = 1, line = list(width = 6, reverscale = FALSE))
+fig <- fig %>% add_trace(name = 'gfem_rear_deform',x = ~gfem_rear_deform_x, y = ~gfem_rear_deform_y, z = ~gfem_rear_deform_z, mode = 'markers', opacity = 1, line = list(width = 6, reverscale = FALSE))
+
+fig <- fig %>% add_trace(name = 'dfem_front_base',x = ~dfem_front_base_x, y = ~dfem_front_base_y, z = ~dfem_front_base_z, mode = 'markers', opacity = 1, line = list(width = 6, reverscale = FALSE))
+fig <- fig %>% add_trace(name = 'dfem_rear_base',x = ~dfem_rear_base_x, y = ~dfem_rear_base_y, z = ~dfem_rear_base_z, mode = 'markers', opacity = 1, line = list(width = 6, reverscale = FALSE))
+fig <- fig %>% add_trace(name = 'dfem_front_deform',x = ~dfem_front_deform_x, y = ~dfem_front_deform_y, z = ~dfem_front_deform_z, mode = 'markers', opacity = 1, line = list(width = 6, reverscale = FALSE))
+fig <- fig %>% add_trace(name = 'dfem_rear_deform',x = ~dfem_rear_deform_x, y = ~dfem_rear_deform_y, z = ~dfem_rear_deform_z, mode = 'markers', opacity = 1, line = list(width = 6, reverscale = FALSE))
+
+
+fig
+
